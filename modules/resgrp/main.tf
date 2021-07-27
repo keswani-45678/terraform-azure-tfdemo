4c7428a76a86387c6bedd869e484a182b40216c6
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-27 16:06:58 +0530

    Update main.tf

--- modules/resgrp/main.tf
+++ modules/resgrp/main.tf
@@ -1,28 +1,18 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${var.location}"
-  resource_group_name = "${var.rgName}"
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  ="${var.rgName}"
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefix     = "10.0.1.0/24"
-
+variable "subscriptionid"{
+  type = string
+  default = "c961a351-ec79-490c-89d5-b31ef157ec0a"
 }
 
 
 resource "azurerm_network_interface" "vmNic" {
   count               = length(var.network_interfaces)
   name                = var.network_interfaces[count.index].name
-  resource_group_name = var.rgName
-  location            = var.location
+  resource_group_name = var.network_interfaces[count.index].rgName
+  location            = var.network_interfaces[count.index].location
 
   ip_configuration {
     name                          = var.network_interfaces[count.index].name
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id            = "/subscriptions/${var.subscriptionid}/resourceGroups/${var.network_interfaces[count.index].rgName}/providers/Microsoft.Network/virtualNetworks/${var.network_interfaces[count.index].vnetname}/subnets/${var.network_interfaces[count.index].subnetName}"
     private_ip_address_allocation = "Static"
     private_ip_address            = var.network_interfaces[count.index].private_ip
   }  
