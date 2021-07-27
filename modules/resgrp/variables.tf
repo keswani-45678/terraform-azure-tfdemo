d62491c0e29b5b71ee36fdaf6de9318f8c89a864
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-27 16:06:33 +0530

    Update variables.tf

--- modules/resgrp/variables.tf
+++ modules/resgrp/variables.tf
@@ -1,50 +1,21 @@
-variable "rgName" {
-  type        = string
-  description = "Specify a resource group name"
-  default = "test1"
-}
-
-variable "location" {
-  type        = string
-  description = "Location for the VM"
-  default = "east us"
-}
-
-variable "vnet_address_space" {
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_prefix" {
-    default = [
-        "10.0.1.0/24",
-        "10.0.2.0/24",
-    ]
-}
-
-variable "vnet_names" {
-  description = "Create Vnets with these names"
-  type        = list(string)
-  default     = ["vnet1", "vnet2"]
-}
-
-variable "subnet_names" {
-  description = "Create subnets with these names"
-  type        = list(string)
-  default     = ["subnet1", "subnet2"]
-}
-
 
 variable "network_interfaces" {
     default = [
         {
-            name = "test1",
-        
-            private_ip = "10.0.1.4"
+            name = "testnic1",
+            rgName = "test1",
+            location = "east us",
+            private_ip = "10.0.1.4",
+            vnetname   = "example-vnet",
+            subnetName = "example-subnet"
         },
         {
-            name = "test2",
-         
-            private_ip = "10.0.1.8"
+            name = "testnic2",
+            rgName = "test1",
+            location = "east us",
+            private_ip = "10.0.1.8",
+            vnetname   = "example-vnet",
+            subnetName = "example-subnet"
         }
     ]
 }
